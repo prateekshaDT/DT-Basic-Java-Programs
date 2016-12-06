4f942f52cd6bd0253330f3ed4f46dc3578c41e84
prateekshaDT <pratheekshagowdaks@gmail.com>
2016-12-06 15:23:28 +0530

    loginapplication

---
+++ LoginApplication/WebContent/Login.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<!--  create a form with 2 fields and 2 buttons -->
+<form action="Home.jsp">
+User Name:<input type="text">
+password:<input type="password">
+<input type="submit">
+<input type="reset">
+</form>
+
+</body>
+</html>
---
+++ LoginApplication/src/com/niit/loginapp/Servlet/loginServlet.java
@@ -0,0 +1,34 @@
+package com.niit.loginapp.Servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class loginServlet
+ */
+//@WebServlet("/LoginServlet")
+public class loginServlet extends HttpServlet {
+	
+   
+  
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	 System.out.println("calling doGet method");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("calling a doPost method");
+	}
+
+}
